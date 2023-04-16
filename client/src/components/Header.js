e241df949970a792fd96bee4db28333cbdadb712
kheller18 <keenanaheller@gmail.com>
2023-04-15 21:41:15 -0700

    small syntax changes and added resume

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,32 +8,36 @@ import '../styles/Header.css';
 const Header = props => {
   const navigate = useNavigate();
 
-  // handles when user clicks cart
+  // handles when user clicks different header options
   const handleClick = (id) => {
     switch (id) {
+      case 'home':
+        navigate('/'); // navigates to home page
+        break;
+
       case 'about':
-        navigate('/about');
+        navigate('/about'); // navigates to about page
         break;
 
       case 'projects':
-        navigate('/projects');
+        navigate('/projects'); // navigates to project page
         break;
 
       default:
-        break;
+        break; // handles invalid route
     };
   };
 
   return (
     <div className='header-container'>
       <div className='header-left'>
-        <h1 className='header-name'>KH</h1>
+        <h1 className='header-name' id='home' onClick={(e => handleClick(e.target.id))}>KH</h1>
       </div>
       <ul className="header-right">
         <li className='header-item' id='about' onClick={(e => handleClick(e.target.id))}>About</li>
         <li className='header-item' id='projects' onClick={(e => handleClick(e.target.id))}>Projects</li>
         <li className='header-item' id='resume'>Resume</li>
-        <FontAwesomeIcon className='header-icon header-item' id='' icon={faCircle} onClick={props.toggleTheme} />
+        <FontAwesomeIcon className='header-icon header-item' id='mode' icon={faCircle} onClick={props.toggleTheme} />
       </ul>
     </div>
   );
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import '../styles/Landing.css';
 
+// component for website home
 const Landing = () => {
 
   return (
