881f41f45da1feee21dfb2b0a3bbb353ea8547f5
kheller18 <keenanaheller@gmail.com>
2023-04-16 16:35:00 -0700

    added project component architecture and added project images

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Image from './Image';
+import '../styles/Projects.css';
 
 const Projects = (props) => {
   return (
     <div className='projects-container'>
-
+      <h1 className='projects-container-header'>Projects</h1>
+      <div className='projects'>
+        <div className='project'>
+          
+        </div>
+      </div>
     </div>
   );
 };
