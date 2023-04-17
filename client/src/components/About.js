5071495afb359bcad5fd8f26158a4b0df3419b58
kheller18 <keenanaheller@gmail.com>
2023-04-16 20:55:11 -0700

    framework in place for about page. content needed

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,9 @@ const About = () => {
   return (
     <div className='about-container'>
       <h1 className='about-container-header'>About</h1>
-
+      <div className='about-details'>
+        <p>Hi! I'm Keenan Heller. Thanks for visiting my site!</p>
+      </div>
     </div>
   );
 };
