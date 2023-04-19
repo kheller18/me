29f93ee77daf4b6c4d73fd6c963ef3b0a9831b69
kheller18 <keenanaheller@gmail.com>
2023-04-19 00:45:23 -0700

    small changes to formatting and content updates

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Image from './Image';
 import burger from '../media/cooking/burger.jpeg';
 import pizza from '../media/cooking/pizza_oven_cropped.png';
@@ -20,39 +21,47 @@ import '../styles/About.css';
 
 // component for about me
 const About = () => {
+  const navigate = useNavigate();
+
+  // const handleRedirect = (e) => {
+  //   switch(e) {
+  //     case
+  //   }
+  // }
+
   return (
     <div className='about-container'>
       <h1 className='about-container-header'>About Me</h1>
       <div className='about-details'>
-        <p className='about-detail'>Hi! I'm Keenan Heller. Thanks for visiting my site!</p>
+        <p className='about-detail no-margin'>Hi, I'm Keenan Heller. Thanks for visiting my site!</p>
         <p className='about-detail'>
-          I studied Mechanical Engineering at the University of Arizona. After graduation, I began my career at Boeing as a Functional Test Manufacturing Engineer for the 777X-8 program,
-          a developmental airplane at the time. My manager was aware of my limited experience in Python and asked me to create a web app that would promote team visibility.  As I started working on the project assigned by my manager,
-          I quickly realized that my true passion lies in software engineering. The process of coding, debugging, and creating software solutions was incredibly fulfilling and enjoyable for me.
-          It ignited a sense of excitement and motivation that I hadn't felt before. I realized that software engineering is where I truly excel and where I want to focus my career moving forward.
-          I'm grateful for the opportunity to work on the project and for my manager, as it helped me discover my true passion and direction in the field of technology.
+          I studied Mechanical Engineering at the University of Arizona. I began my career at Boeing as a Functional Test Manufacturing Engineer for the 777X-8 program,
+          a developmental airplane at the time. My manager was aware of my limited experience in Python and asked me to create a web app that would promote team visibility.
+          As I started working on the project assigned by my manager, I quickly realized that my true passion lies in software engineering. The process of coding, debugging, and creating
+          software solutions was incredibly fulfilling and enjoyable for me. It ignited a sense of excitement and motivation that I hadn't ∂felt before. I realized that software engineering
+          is where I truly excel and where I want to focus my career moving forward. I'm grateful for the opportunity to work on the project and for my manager, as it helped me discover my true
+          passion and direction in the field of technology.
         </p>
         <p className='about-detail'>
           Since this time, I’ve completed two bootcamp certificate programs from the University of Washington in Full Stack Web Development and FinTech.
           As the world of software engineering evolves rapidly, with new technologies, methodologies, and best practices emerging constantly, my unwavering desire to continually learn,
-          improve and understand the “why” will be the key to my success. In this ever-changing landscape, staying stagnant is not an option. To thrive, I recognize the need to adapt, grow, and constantly
-          upgrade my skills to stay ahead of the curve. I am committed to staying curious, embracing challenges, and seeking out opportunities for professional development. With a growth mindset and a proactive approach to learning,
-          I am confident that I will excel in the dynamic field of software engineering and contribute to cutting-edge innovations that shape the future of technology. Keep up with my projects and see what I’m working on!
+          improve and understand the “why” will be the key to my success. In this ever-changing landscape, staying stagnant is not an option. To thrive, I recognize the need to adapt, grow,
+          and constantly upgrade my skills to stay ahead of the curve. I am committed to staying curious, embracing challenges, and seeking out opportunities for professional development.
+          With a growth mindset and a proactive approach to learning, I am confident that I will excel in the dynamic field of software engineering and contribute to cutting-edge innovations
+          that shape the future of technology. Keep up with my projects and see what I’m working on!
         </p>
         <p className='about-detail'>
           Outside of software development, you can find me in the kitchen, outside, around the world and following my favorite teams.
-        </p>
-        <p className='about-detail'>
           Cooking has evolved from a simple task to a form of artistic expression that is truly soothing for my soul. With each dish, I embark on a culinary journey, carefully selecting ingredients,
-          experimenting with flavors and textures, and creating a symphony of colors and aromas. The kitchen has become my canvas, and cooking has become my art form. As I chop, sauté, and simmer, I am transported
-          to a world of creativity and inspiration, where my imagination and senses come alive. The act of cooking has become a therapeutic ritual, allowing me to pour my heart and soul into each dish, and share my passion with others.
-          From the precise plating to the first bite, the joy and satisfaction I derive from cooking is unparalleled, filling my heart with warmth and contentment.
-          Cooking has truly become a masterpiece that brings me solace, nourishment, endless inspiration for my soul and a way to connect through traveling.
+          experimenting with flavors and textures, and creating a symphony of colors and aromas. The kitchen has become my canvas, and cooking has become my art form. The act of cooking has become
+          a therapeutic ritual, allowing me to pour my heart and soul into each dish, and share my passion with others. From the precise plating to the first bite,the joy and satisfaction I derive
+          from cooking is unparalleled, filling my heart with warmth and contentment. Cooking has truly become a masterpiece that brings me solace, nourishment, endless inspiration for my soul and
+          a way to connect through traveling.
         </p>
         <p className='about-detail'>
-          Through my experiences with traveling, I have been able to broaden my horizons and gain a deeper understanding of the world around me. Exploring new cultures, meeting diverse people, and
-          immersing myself in different environments have opened my mind to new perspectives, enriched my life with unique experiences and fostered personal growth. Traveling has truly been a transformative journey that
-          has expanded my horizons in ways I never imagined possible.
+          Through my experiences with traveling, I have been able to broaden my horizons and gain a deeper understanding of the world around me. Exploring new cultures, meeting diverse people,
+          andimmersing myself in different environments have opened my mind to new perspectives, enriched my life with unique experiences and fostered personal growth. Traveling has truly
+          been a transformative journey that has expanded my horizons in ways I never imagined possible.
         </p>
         <p className='about-detail'>
           Check out some of my favorite content below and thanks again for visiting!
@@ -65,12 +74,16 @@ const About = () => {
           <Image className='about-img' src={iceCream} />
           <Image className='about-img' src={pizza} />
         </div>
+      </div>
+      <div className='about-details'>
         <h1 className='about-container-header'>Traveling</h1>
         <div className='about-img-row'>
           <Image className='about-img' src={gondola} />
           <Image className='about-img' src={goats} />
           <Image className='about-img' src={luzern} />
         </div>
+      </div>
+      <div className='about-details'>
 
         <h1 className='about-container-header'>Teams I Love</h1>
         <div className='about-img-row'>
@@ -80,6 +93,8 @@ const About = () => {
           <Image className='about-img-team' src={sounders} />
           <Image className='about-img-team' src={sonics} />
         </div>
+      </div>
+      <div className='about-details'>
 
         <h1 className='about-container-header'>Albums on Repeat</h1>
         <div className='about-img-row'>
