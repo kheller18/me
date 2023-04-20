8ff747cbc1a7c0dcd8ddffc8a503764c83fac991
kheller18 <keenanaheller@gmail.com>
2023-04-19 20:20:18 -0700

    updated styling throughout app and added fallback route for bad url

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Landing from './components/Landing';
 import About from './components/About';
@@ -60,6 +60,7 @@ function App() {
             <Route path='/about' exact element={<About />} />
             <Route path='/projects' exact element={<Projects />} />
             <Route path='/contact' exact element={<Contact />} />
+            <Route path='*' element={<Navigate to='/' />} />
           </Routes>
           }
         </div>
