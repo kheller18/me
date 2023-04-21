80a25705f8e2427577f32f00227df96b2fa34514
kheller18 <keenanaheller@gmail.com>
2023-04-20 18:19:33 -0700

    added media breakpoints and created context for app theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Landing from './components/Landing';
@@ -9,11 +9,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from  '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
+export const ThemeContext = createContext();
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [theme, setTheme] = useState(
     localStorage.getItem('theme') || 'dark'
   );
+
+  // ThemeContext = React.createContext(theme);
 
   const toggleTheme = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light');
@@ -46,26 +50,49 @@ function App() {
   }, [theme]);
 
   return (
-    <Router>
-      <div className={`App ${theme}`}>
-        <Header toggleTheme={toggleTheme} />
-        <div className='App-body'>
-          {loading ?
-          <div>
-            <FontAwesomeIcon icon={faSpinner} spin />
+    <ThemeContext.Provider value={theme}>
+      <Router>
+        <div className={`App ${theme}`}>
+          <Header toggleTheme={toggleTheme} />
+          <div className='App-body'>
+            {loading ?
+            <div>
+              <FontAwesomeIcon icon={faSpinner} spin />
+            </div>
+            :
+            <Routes>
+              <Route path='/' exact element={<Landing />} />
+              <Route path='/about' exact element={<About />} />
+              <Route path='/projects' exact element={<Projects />} />
+              <Route path='/contact' exact element={<Contact />} />
+              <Route path='*' element={<Navigate to='/' />} />
+            </Routes>
+            }
           </div>
-          :
-          <Routes>
-            <Route path='/' exact element={<Landing />} />
-            <Route path='/about' exact element={<About />} />
-            <Route path='/projects' exact element={<Projects />} />
-            <Route path='/contact' exact element={<Contact />} />
-            <Route path='*' element={<Navigate to='/' />} />
-          </Routes>
-          }
         </div>
-      </div>
-    </Router>
+      </Router>
+
+    </ThemeContext.Provider>
+    // <Router>
+    //   <div className={`App ${theme}`}>
+    //     <Header toggleTheme={toggleTheme} />
+    //     <div className='App-body'>
+    //       {loading ?
+    //       <div>
+    //         <FontAwesomeIcon icon={faSpinner} spin />
+    //       </div>
+    //       :
+    //       <Routes>
+    //         <Route path='/' exact element={<Landing />} />
+    //         <Route path='/about' exact element={<About />} />
+    //         <Route path='/projects' exact element={<Projects />} />
+    //         <Route path='/contact' exact element={<Contact />} />
+    //         <Route path='*' element={<Navigate to='/' />} />
+    //       </Routes>
+    //       }
+    //     </div>
+    //   </div>
+    // </Router>
   );
 };
 
