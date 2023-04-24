59f8bf8cc0a134612935f9e890d744681f47eac6
kheller18 <keenanaheller@gmail.com>
2023-04-24 14:51:29 -0700

    added comments

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 
-// defining the port for heroku or local
+// defining the port for aws or local
 const PORT = process.env.PORT || 3001;
 
 // initializing app
